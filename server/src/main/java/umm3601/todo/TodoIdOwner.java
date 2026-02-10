7b3388ea4285c131bcbe65f857b8b04a14b34700
max matejka <matej065@morris.umn.edu>
2026-02-10 14:16:11 -0600

    added folders from Lab 2, and started on Tests
    Co-authored-by: Elijah Slinden <slind054@morris.umn.edu>

---
+++ server/src/main/java/umm3601/todo/TodoIdOwner.java
@@ -0,0 +1,8 @@
+package umm3601.todo;
+
+@SuppressWarnings("checkstyle:visibilitymodifier")
+public class TodoIdOwner {
+  @SuppressWarnings("checkstyle:membername")
+  public String _id;
+  public String owner;
+}
