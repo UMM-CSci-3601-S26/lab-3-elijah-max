03991bc800dc2e33938f8f7fe0ca051444774a86
max matejka <matej065@morris.umn.edu>
2026-02-17 14:23:12 -0600

    started the todo e2e

---
+++ client/cypress/e2e/ add-todo.cy.ts
@@ -0,0 +1,25 @@
+import { Todo } from 'src/app/todos/todo';
+import { AddTodoPage } from '../support/add-todo.po';
+
+describe('Add todo', () => {
+  const page = new AddTodoPage();
+
+  beforeEach(() => {
+    page.navigateTo();
+  });
+
+  it('Should have the correct title', () => {
+    page.getTitle().should('have.text', 'New Todo');
+  });
+
+  it('Should enable and disable the add todo button', () => {
+    page.addTodoButton().should('be.disabled');
+    page.getFormField('owner').type('test');
+    page.addTodoButton().should('be.disabled');
+    page.getFormField('status').type('complete');
+    page.getTodoButton().should('be.disabled');
+    page.getFormField('body').type('invalid');
+    page.getTodoButton().should('be.disabled');
+    page.getFormField('body').type('')
+  })
+}
